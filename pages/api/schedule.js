6bc4e4d649667d4755bdca6b00d3cb357d2555ef
K <kay-is@users.noreply.github.com>
2022-08-05 12:50:32 +0000

    Init

--- pages/api/schedule.js
+++ pages/api/schedule.js
@@ -3,15 +3,11 @@ import { Client } from "@upstash/qstash"
 const qstashClient = new Client({ token: process.env.QSTASH_TOKEN })
 
 export default async function handler(request, response) {
-  if (request.method !== "POST") return response.end()
-
-  const { host } = request.headers
-  console.log(request.body)
-  const body = JSON.parse(request.body)
+  if (request.method !== "POST") return response.status(404).end()
 
   const qstashResponse = await qstashClient.publishJSON({
-    url: `https://${host}/api/notify`,
-    body,
+    url: `https://${request.headers.host}/api/notify`,
+    body: request.body,
   })
 
   response.status(201).send(qstashResponse)
