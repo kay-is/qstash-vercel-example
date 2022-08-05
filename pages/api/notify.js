e64a3a2af6936acae3d3152b109778f90ca1c08d
K <kay-is@users.noreply.github.com>
2022-08-05 16:34:39 +0000

    Init

--- pages/api/notify.js
+++ pages/api/notify.js
@@ -1,26 +1,15 @@
-import { Receiver } from "@upstash/qstash"
-import { text } from "micro"
+import { verifySignature } from "@upstash/qstash/nextjs"
 
 export const config = {
   api: { bodyParser: false },
 }
 
-const qstashReceiver = new Receiver({
+async function handler(request, response) {
+  console.log(request.body)
+  response.send("OK")
+}
+
+export default verifySignature(handler, {
   currentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY,
   nextSigningKey: process.env.QSTASH_NEXT_SIGNING_KEY,
 })
-
-export default async function handler(request, response) {
-  const body = await text(request)
-
-  console.log(body)
-
-  const isValid = await qstashReceiver.verify({
-    signature: request.headers["Upstash-Signature"],
-    body,
-  })
-
-  console.log("Is Valid:", isValid)
-
-  response.end(isValid)
-}
